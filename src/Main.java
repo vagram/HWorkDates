ba6a111badbf9637f1a733dc7db341032a9e5342
Vagram_ <vdourmychev@gmail.com>
2022-04-21 12:04:03 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,38 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        LocalDate startDate = LocalDate.of(2018, 11, 12);
-        LocalDate endDate = LocalDate.of(2018, 11, 20);
-        System.out.println();
-        System.out.println(endDate);
-        startDate = startDate.minusDays(1);
-        while (startDate.isBefore(endDate)) {
-            startDate = startDate.plusDays(1);
-            if (startDate.getDayOfWeek().getValue() <= 5) {
-                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMMM EEEE yyyy");
-                String temp = startDate.format(dateFormat);
-                System.out.println(temp);
-            }
-        }
+    public static LocalDate date1;
+    public static LocalDate date2;
+    public static String temp;
 
+    public static void main(String[] args) {
+        enterDates();
+        weekDays();
+    }
+
+    public static void enterDates() {
+        System.out.println("Введите даты: ");
+        String startDate = new Scanner(System.in).nextLine();
+        String endDate = new Scanner(System.in).nextLine();
+        date1 = LocalDate.parse(startDate.formatted());
+        date2 = LocalDate.parse(endDate.formatted());
+    }
+
+    public static void weekDays() {
+        while (date1.isBefore(date2)) {
+            if (date1.getDayOfWeek().getValue() <= 5) {
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMMM EEEE yyyy");
+                temp = date1.format(dateFormat);
+                print();
+            }
+            date1 = date1.plusDays(1);
+        }
+    }
+
+    public static void print() {
+        System.out.println(temp);
     }
 }
 
